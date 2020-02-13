3203cb713b6d30921aeaf3e9fe8fae85b928411f
Coaco <naseeihility@163.com>
2020-02-13 15:34:14 +0800

    Binary Search

---
+++ search/69.sqrt-x.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+# 二分查找
+
+# @lc code=start
+
+
+class Solution:
+    def mySqrt(self, x):
+        if x == 0 or x == 1:
+            return x
+        l = 1
+        r = x
+        res = 1
+
+        while (l <= r):
+            mid = int((l + r) / 2)
+            if mid == int((x / mid)):
+                return int(mid)
+            elif mid > int((x / mid)):
+                r = mid - 1
+            else:
+                l = mid + 1
+                res = mid
+        return int(res)
+        # @lc code=end
